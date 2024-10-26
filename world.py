41c02d97d6066e10a98d1a5fca99da4317289ab7
arshanadevi <122380014+arshanadevi@users.noreply.github.com>
2024-10-26 08:28:39 +0200

    Add files via upload

--- world.py
+++ world.py
@@ -1,26 +1,22 @@
-from __future__ import annotations
-
-import pygame
-
-from minigrid.core.constants import COLOR_NAMES  # type: ignore
-from minigrid.core.grid import Grid  # type: ignore
-from minigrid.core.mission import MissionSpace  # type: ignore
-from minigrid.core.world_object import Goal, Wall  # type: ignore
-from minigrid.manual_control import ManualControl  # type: ignore
-from minigrid.minigrid_env import MiniGridEnv  # type: ignore
+from minigrid.core.constants import COLOR_NAMES
+from minigrid.core.grid import Grid
+from minigrid.core.mission import MissionSpace
+from minigrid.core.world_object import Goal, Key, Wall
+from minigrid.minigrid_env import MiniGridEnv 
+from typing import Optional
 
 from DFSMaze import MazeGenerator
 
+import numpy as np
 
-# taken from https://minigrid.farama.org/
 class SimpleEnv(MiniGridEnv):
     def __init__(
         self,
         size=10,
-        # agent_start_pos=(1, 1),
-        agent_start_pos=None,
+        agent_start_pos=(1, 1),
         agent_start_dir=0,
-        max_steps: int | None = None,
+        # max_steps: int | None = None,
+        max_steps: Optional[int] = None,
         **kwargs,
     ):
         self.agent_start_pos = agent_start_pos
@@ -31,70 +27,74 @@ class SimpleEnv(MiniGridEnv):
         generator = MazeGenerator()
         self.maze = generator.generate_maze()
 
+        self.key_available = [True, True]
+        
+        
         if max_steps is None:
             max_steps = 4 * size**2
 
         super().__init__(
             mission_space=mission_space,
             grid_size=size,
-            # Set this to True for maximum speed
             see_through_walls=True,
             max_steps=max_steps,
             **kwargs,
         )
 
-    def _gen_grid(self, width, height):
-        # Create an empty grid
-        self.grid = self.grid or Grid(width, height)
-
-        # Generate the maze from the static layout
-        for i in range(10):
-            for j in range(10):
-                if self.maze[i][j] == 1:
-                    self.grid.set(j, i, Wall())  # Set wall
-                elif self.maze[i][j] == 2:
-                    self.agent_start_pos = (j, i)  # Set agent's start position
-                elif self.maze[i][j] == 3:
-                    self.grid.set(j, i, Goal())  # Sub-goal
-                elif self.maze[i][j] == 4:
-                    self.grid.set(j, i, Goal())  # End goal
-
-        # Set agent position
-        if self.agent_start_pos:
-            self.agent_pos = self.agent_start_pos
-            self.agent_dir = 0  # Default agent direction
-
     @staticmethod
     def _gen_mission():
-        return "Reach sub-goal and then end-goal"
+        return ""
 
-    # def _gen_grid(self, width, height):
-    #     # Create an empty grid
-    #     self.grid = Grid(width, height)
+    def _gen_grid(self, width, height):        
+        
+        self.grid = Grid(width, height)
+        
+        #generate visuals from grid
+        for i in range(0,10):
+            for t in range(0,10):
+                if self.maze[i][t] == 1:
+                    self.grid.set(t, i, Wall())
+                if self.maze[i][t] == 3:                    
+                    self.grid.set(t, i, Key(COLOR_NAMES[0]))    
+                    self.subgoal_pos = np.ndarray((2,), buffer=np.array([t, i]), dtype=int)                           
+                if self.maze[i][t] == 4:
+                    #maybe set door as well later on?
+                    #self.grid.set(t, i, Door(COLOR_NAMES[0], is_locked=True))
+                    pass
+                if self.maze[i][t] == 5:                    
+                    self.grid.set(t, i, Goal())                    
+        
+        if self.agent_start_pos is not None:
+            self.agent_pos = self.agent_start_pos
+            self.agent_dir = self.agent_start_dir
+        else:
+            self.place_agent()
 
-    #     # Generate the surrounding walls
-    #     #self.grid.wall_rect(0, 0, width, height)
-
-    #     #generate visuals from grid
-    #     print(self.maze)
-    #     for i in range(0,10):
-    #         for t in range(0,10):
-    #             if self.maze[i][t] == 1:
-    #                 self.grid.set(t, i, Wall())
-                                    
-        # Generate vertical separation wall
-        # for i in range(0, height):
-        #     self.grid.set(5, i, Wall())
-
-
-def main():
-    pygame.init()
-    env = SimpleEnv(render_mode="human")
-
-    # enable manual control for testing
-    manual_control = ManualControl(env, seed=42)
-    manual_control.start()
-
+        self.mission = "grand mission"
     
-if __name__ == "__main__":
-    main()
+    # overload the step function, so we can add some rules and rewards
+    def step(self, action):
+        obs, reward, terminated, truncated, info = super().step(action)
+
+        # store truth values whether the key is available for pickup,
+        # as we will be behind a step
+        self.key_available[0] = self.key_available[1]
+        self.key_available[1] = self.grid.get(*self.subgoal_pos) is not None
+
+        # if pickup action is used, and the key is available and the agent standing in front of it,
+        # pick the key up 
+        if (action == self.actions.pickup and 
+            (self.front_pos == self.subgoal_pos).all() and 
+            self.key_available[0]):
+
+            reward = self._reward() / 1.5
+        
+        #prevent the agent to drop the key or toggle
+        if action == self.actions.drop: #or action == self.actions.toggle:
+            terminated = True
+
+        return obs, reward, terminated, truncated, info
+
+
+
+
